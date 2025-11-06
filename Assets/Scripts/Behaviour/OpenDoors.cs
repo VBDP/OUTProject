9722b9aab661a4055cf9c609fee603eb4ca1923e
VBDP <vbd@vbd.es>
2025-11-06 11:42:26 +0100

    Script para abrir las puertas básico creado y funcional

---
+++ Assets/Scripts/Behaviour/OpenDoors.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class OpenDoors : MonoBehaviour
+{
+    private bool isOpen = false;                
+    private Quaternion closedRotation;          
+    private Quaternion openRotation;            
+    private float rotationSpeed = 1.5f;            
+
+    void Start()
+    {
+        
+        closedRotation = transform.rotation;
+        openRotation = Quaternion.Euler(0, 90, 0) * closedRotation; 
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Has hecho click en la puerta");
+        isOpen = !isOpen; 
+    }
+
+    void Update()
+    {
+       
+        if (isOpen)
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, openRotation, Time.deltaTime * rotationSpeed);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, closedRotation, Time.deltaTime * rotationSpeed);
+        }
+    }
+}
